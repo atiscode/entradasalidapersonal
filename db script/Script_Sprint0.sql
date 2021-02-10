b3bc4d8868c132e58f80f52983b4746bd2af2fc0
carliwar <basscarland@hotmail.com>
2021-02-10 12:03:14 -0500

    Tarea 3. "Estado Civil: Aumentar en el catálogo ""Unión de Hecho"""

--- db script/Script_Sprint0.sql
+++ db script/Script_Sprint0.sql
@@ -1,4 +1,6 @@
-/****************************************************
+USE RRHH
+GO
+/*****************  Tarea 1  ************************
 	Proyecto: RRHH									*
 	Fecha: 09/feb/2021
 	Descripción: Empresa: Retirar del listado Finzo, 
@@ -9,3 +11,13 @@
 DELETE FROM adm.Catalogo WHERE NombreCatalogo IN ('FINZO')
 UPDATE adm.Catalogo SET NombreCatalogo = 'KICKOFF', CodigoCatalogo = 'KICKOFF' WHERE CodigoCatalogo = 'PTV'
 UPDATE adm.Catalogo SET NombreCatalogo = 'OPINO', CodigoCatalogo = 'OPINO' WHERE CodigoCatalogo = 'OSTB'
+GO
+/*****************  Tarea 3  ************************
+	Proyecto: RRHH									*
+	Fecha: 10/feb/2021
+	Descripción: "Estado Civil: Aumentar en el catálogo 
+	""Unión de Hecho"""
+*****************************************************/
+INSERT INTO adm.Catalogo (CodigoCatalogo, NombreCatalogo, DescripcionCatalogo, IdCatalogoPadre, EstadoCatalogo,IdEmpresa,Eliminado)
+VALUES (NULL,'UNIÓN DE HECHO','UNIÓN DE HECHO', 123, 1, 1, 0)
+GO
