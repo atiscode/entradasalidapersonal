2f138df8ea95da8f5e526d8838d4e83f2b870644
carliwar <basscarland@hotmail.com>
2021-02-10 11:05:01 -0500

    2. "Bancos: Si escoge el usuario un banco diferente al banco del Pichincha, debe salir un mensaje que indique ""Por favor es necesario aperturar una cuenta en el Banco del Pichincha"" // Debe dejar guardar a pesar de que no tenga Banco Pichincha"

---
+++ EntradaSalidaRRHH.UI/Configs/Configurations.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+namespace EntradaSalidaRRHH.UI.Configs
+{
+    public static class Configurations
+    {
+        public  static string GetBancoDefault()
+        {
+            return ConfigurationManager.AppSettings["BancoDefault"];
+        }
+    }
+}
